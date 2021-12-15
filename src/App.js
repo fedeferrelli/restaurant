b36325c9a583bf673e36b3144285c95345cb3eff
Fede <ferrelli.mazza@gmail.com>
2021-12-14 22:33:14 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/main.css';
+
+function App() {
+  return (
+    <p  className="text-rose-800" /* style={{ height: 10, color:'orange' }} */> 
+      Hola Mundo Cruel
+    </p>
+    
+  );
+}
+
+export default App;
